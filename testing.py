0e17f8e5b25d9517ae5b152a2e22593dcccffe5f
Yenneti <Swati.Yenneti@McKesson.com>
2018-07-22 01:15:51 -0500

    2nd commit

--- testing.py
+++ testing.py
@@ -1,79 +1,79 @@
 import assessment as a
 import numpy as np
 import pandas as pd
-#import pytest
+import pytest
 
 
-# def test_count_characters():
+def test_count_characters():
 
-#          string = "abafdcggfaabe"
-#          answer = {"a": 4, "b": 2, "c": 1, "d": 1, "e": 1, "f": 2, "g": 2}
-#          result = a.count_characters(string)
-#          print(result)
-#          assert result == answer
-
-
-# def test_invert_dictionary():
-#     d = {"a": 4, "b": 2, "c": 1, "d": 1, "e": 1, "f": 2, "g": 2}
-#     result = {4: {'a'}, 2: {'b', 'f', 'g'}, 1: {'c', 'd', 'e'}}
-#     assert a.invert_dictionary(d) == result
+         string = "abafdcggfaabe"
+         answer = {"a": 4, "b": 2, "c": 1, "d": 1, "e": 1, "f": 2, "g": 2}
+         result = a.count_characters(string)
+         print(result)
+         assert result == answer
 
 
-# def test_word_count():
-#     assert a.word_count('data/alice.txt') == (17, 1615, 8461)
+def test_invert_dictionary():
+    d = {"a": 4, "b": 2, "c": 1, "d": 1, "e": 1, "f": 2, "g": 2}
+    result = {4: {'a'}, 2: {'b', 'f', 'g'}, 1: {'c', 'd', 'e'}}
+    assert a.invert_dictionary(d) == result
 
 
-# def test_matrix_multiplication():
-#             A = [[2, 3, 4], [6, 4, 2], [-1, 2, 0]]
-#             B = [[8, -3, 1], [-7, 3, 2], [0, 3, 3]]
-#             answer = [[-5, 15, 20], [20, 0, 20], [-22, 9, 3]]
-#             assert a.matrix_multiplication(A, B) == answer
+def test_word_count():
+    assert a.word_count('data/alice.txt') == (17, 1615, 8461)
 
 
-# def test_array_work():
-#             matrixA = np.array([[-4, -2],
-#                                 [0, -3],
-#                                 [-4, -1],
-#                                 [-1, 1],
-#                                 [-3, 0]])
-#             answer1 = np.array([[-24, -24, -24],
-#                                 [-12, -12, -12],
-#                                 [-20, -20, -20],
-#                                 [0, 0, 0],
-#                                 [-12, -12, -12]])
-#             result1 = a.array_work(2, 3, 4, matrixA)
-#             assert np.all(answer1 == result1)
-
-#             answer2 = np.array([[-36, -36],
-#                                 [-18, -18],
-#                                 [-30, -30],
-#                                 [0, 0],
-#                                 [-18, -18]])
-#             result2 = a.array_work(2, 2, 6, matrixA)
-#             assert np.all(answer2 == result2)
-
-# def test_boolean_indexing():
-#             arr = np.array([[-4, -4, -3],
-#                             [-1, 16, -4],
-#                             [-3, 6, 4]])
-#             result1 = a.boolean_indexing(arr, 0)
-#             answer1 = np.array([16, 6, 4])
-#             assert np.all(result1 == answer1)
-#             result2 = a.boolean_indexing(arr, 10)
-#             answer2 = np.array([16])
-#             assert np.all(result2 == answer2)
+def test_matrix_multiplication():
+            A = [[2, 3, 4], [6, 4, 2], [-1, 2, 0]]
+            B = [[8, -3, 1], [-7, 3, 2], [0, 3, 3]]
+            answer = [[-5, 15, 20], [20, 0, 20], [-22, 9, 3]]
+            assert a.matrix_multiplication(A, B) == answer
 
 
-# def test_make_series():
-#             result = a.make_series(7, 4, ['a', 'b', 'c', 'd'])
-#             assert isinstance(result, pd.Series)
-#             assert result['a'] == 7
-#             assert result['d'] == 10
+def test_array_work():
+            matrixA = np.array([[-4, -2],
+                                [0, -3],
+                                [-4, -1],
+                                [-1, 1],
+                                [-3, 0]])
+            answer1 = np.array([[-24, -24, -24],
+                                [-12, -12, -12],
+                                [-20, -20, -20],
+                                [0, 0, 0],
+                                [-12, -12, -12]])
+            result1 = a.array_work(2, 3, 4, matrixA)
+            assert np.all(answer1 == result1)
 
-#             result = a.make_series(22, 5, ['a', 'b', 'c', 'd', 'hi'])
-#             assert result['a'] == 22
-#             assert result['d'] == 25
-#             assert result['hi'] == 26
+            answer2 = np.array([[-36, -36],
+                                [-18, -18],
+                                [-30, -30],
+                                [0, 0],
+                                [-18, -18]])
+            result2 = a.array_work(2, 2, 6, matrixA)
+            assert np.all(answer2 == result2)
+
+def test_boolean_indexing():
+            arr = np.array([[-4, -4, -3],
+                            [-1, 16, -4],
+                            [-3, 6, 4]])
+            result1 = a.boolean_indexing(arr, 0)
+            answer1 = np.array([16, 6, 4])
+            assert np.all(result1 == answer1)
+            result2 = a.boolean_indexing(arr, 10)
+            answer2 = np.array([16])
+            assert np.all(result2 == answer2)
+
+
+def test_make_series():
+            result = a.make_series(7, 4, ['a', 'b', 'c', 'd'])
+            assert isinstance(result, pd.Series)
+            assert result['a'] == 7
+            assert result['d'] == 10
+
+            result = a.make_series(22, 5, ['a', 'b', 'c', 'd', 'hi'])
+            assert result['a'] == 22
+            assert result['d'] == 25
+            assert result['hi'] == 26
 
 
 def test_data_frame_work():
